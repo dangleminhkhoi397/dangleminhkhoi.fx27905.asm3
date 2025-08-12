c888a4bbe8fd6afa7ab0627d001932e54d2daf18
dangleminhkhoi397 <khoidlmFX27905@funix.edu.vn>
2025-08-12 16:06:14 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
+// mail box function
+
+const regex =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 document.getElementById("myForm").addEventListener("submit", function (event) {
   event.preventDefault();
 
   var email = document.getElementById("email").value;
   var infoContainer = document.getElementById("infoContainer");
 
-  if (email === "" || !email.includes("@")) {
+  if (email === "" || !regex.test(email)) {
     alert("Email không hợp lệ. Vui lòng nhập lại!");
   } else {
     this.classList.add("hide");
@@ -12,29 +17,27 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
   }
 });
 
+//info function
+
 var skills = document.getElementsByClassName("mom");
 
-for (var i = 0; i < skills.length; i++) {
-  var button = skills[i].querySelector(".btn");
+for (let skill of skills) {
+  const button = skill.querySelector(".btn");
+  const content = skill.querySelector(".job-info-content");
   button.style.display = "none";
 
-  skills[i].addEventListener("mouseover", function () {
-    var button = this.querySelector(".btn");
+  skill.addEventListener("mouseover", () => {
     button.style.display = "inline";
   });
 
-  skills[i].addEventListener("mouseout", function () {
-    var content = this.querySelector(".job-info-content");
-    var button = this.querySelector(".btn");
-    if (content.style.display === "none") {
+  skill.addEventListener("mouseout", () => {
+    if (content.style.display !== "block") {
       button.style.display = "none";
     }
   });
 
-  skills[i].addEventListener("click", function () {
-    var content = this.querySelector(".job-info-content");
-    var button = this.querySelector(".btn");
-    if (content.style.display === "none") {
+  skill.addEventListener("click", () => {
+    if (content.style.display === "none" || !content.style.display) {
       content.style.display = "block";
       button.textContent = "VIEW LESS";
     } else {
